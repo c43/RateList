bcba9a401fb7390412e879096c9ddfc0c9131bd7
Cameron Donahue <camerondonahue@gmail.com>
2014-09-10 17:57:52 -0600

    users can add to list, edit & delete itmes.  Placeholder for login system

--- js/script.js
+++ js/script.js
@@ -1,62 +1,91 @@
 $(function() {
 
+
 	$('.fires .fire').hover(
 		function(){
-			$(this).addClass('filled').prevAll().addClass('filled');}
+
+			$(this).addClass('filled').prevAll().addClass('filled');
+
+			colorRating($('.fires .filled').length);
+
+			}
 		, function(){
-			$(this).removeClass('filled').prevAll().removeClass('filled');
+			$('.fires .filled').removeAttr("style")
+			$('.fires .filled').removeClass('filled');
 		});
 		
 
-	$('.fires .fire').on('click', function() {
-		setRating($(this));
-	});
-
-	setupForm();
-	setupTable();
-
-
-})
-
-
-
-function setRating(obj){
-	if (obj.hasClass('rated') && !obj.next().hasClass('rated')){
-		obj.removeClass('rated').siblings().removeClass('rated');
-	}
-	else {
-		obj.removeClass('rated').siblings().removeClass('rated');
-		obj.addClass('rated').prevAll().addClass('rated');
-	}
-};
-
-function setupForm() {
-	$('form').on('submit', function(event){
+	$('.fires .fire').on('click', function(event) {
+		
 		event.preventDefault();
 		processNewItem();
-	});
-};
 
-function setupTable() {
+		//colorRating($(this));
+		//$(this).removeAttr('opacity')
+	});
+
+
+	trashItem();
+
+
+});
+
+
+function colorRating(obj) {
+		console.log(obj)
+		// 'obj' goes from 2-6, one for each fire object -- the text "Rate it:" is getting counted as an object.
+		if(obj === 2){
+				$('.filled').css(
+					"color", "#843");
+			}
+		else if(obj === 3){
+				$('.filled').css(
+					"color", "#EB0");
+			}
+		else if(obj === 4){
+				$('.filled').css(
+					"color", "#FE642E");
+			}
+		else if(obj === 5){
+				$('.filled').css(
+					"color", "#F44");
+			}
+		else if(obj === 6){
+				$('.filled').css(
+					"color", "#3333FF"
+					)
+			}
+
+		// $('#items .fire').animate({
+		// opacity: .2
+		// });
+		// obj.removeClass('rated').siblings().removeClass('rated');
+		// obj.addClass('rated').prevAll().addClass('rated');
+}
+
+
+function trashItem() {
 	$('#items').on('click', function(event){
 		var t = event.target;
-		if($(t).hasClass('glyphicon-trash')){
-			$(t).closest('tr').remove();
+		if($(t).hasClass('delRow')){
+			$(t).closest('tr').fadeOut(200);
 		}
-	})
+	});
 }
 
 function processNewItem() {
 	var text = $('#firstItemName').val();
-	var rating = $('.fires .rated').length;
-	addItem(text,rating);
+	var rating = $('.fires .filled').length;
+	var color = $('.filled').css("color");
+	addItem(text,rating,color);
 }
 
-function addItem(text,rating) {
-    $('.template tr').clone().appendTo('#items tbody');
+function addItem(text,rating, color) {
+    $('.template tr').clone().hide().appendTo('#items tbody').fadeIn();
     $('#items tbody tr:last-child td.itemText').text(text);
     var newRating = $('#items tbody tr:last-child td.rating .fire');
-    for(var i = rating - 1; i >= 0; i--) {
-        $(newRating[i]).addClass('rated');
+    //rating - 2 because the label is included in the .fires .filled class, causing an extra element in the .length
+    for(var i = rating - 2; i >= 0; i--) {
+        $(newRating[i]).css("color",color)
     }
 }
